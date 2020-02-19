cdd66724c08a50cae772fc4a20b72d4171d8c6ce
Hannah Thorpe <hthorpe@protonmail.com>
2020-02-19 11:39:01 -0500

    fifth exercise complete

--- scripts/store.js
+++ scripts/store.js
@@ -4,7 +4,46 @@ let items = [];
 let hideCheckedItems = false;
 
 
+function findByID(id){
+ items.find(id);
+ return id;
+}
+
+function addItem(name){
+  try{
+    item.validateName(name);
+    this.items.push(item.create(name));
+  }
+  catch(error){
+    console.log('This is wrong!');
+  }
+}
+
+function findAndToggleChecked(id){
+  let toggle = this.findByID(id);
+  toggle.checked = !toggle.checked;
+}
+
+function findAndUpdateName(id, newName){
+  try{
+    item.validateName(newName);
+    this.findByID(id);
+  }
+  catch(error){
+    console.log(`Cannot update name: ${error.message}`);
+  }
+}
+
+function findAndDelete(id) {
+  this.items=this.items.filter(item => item.id !==id);
+}
+
 export default{
   items,
-  hideCheckedItems
+ hideCheckedItems,
+ findByID,
+ addItem,
+ findAndToggleChecked,
+ findAndUpdateName,
+ findAndDelete,
 };
